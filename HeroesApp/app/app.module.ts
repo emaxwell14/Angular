56d76937c96b0fae0007c7f026459a7a3622d363
emaxwell14 <emaxwell1414@hotmail.com>
2016-10-17 18:23:33 +0100

    Added search using observable. Finished tutorial.

--- HeroesApp/app/app.module.ts
+++ HeroesApp/app/app.module.ts
@@ -8,8 +8,12 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 import { DashboardComponent }  from './dashboard.component';
+import { HeroSearchComponent }  from './hero-search.component';
 
 import { AppRoutingModule }     from './app-routing.module';
+
+//Better practive to import these only in the files where they are required.
+import './rxjs-extensions';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
@@ -27,7 +31,7 @@ import { InMemoryDataService }  from './in-memory-data.service';
   // So component selector tags are recognised by Angular
   // i.e. Directives in module are available to other directives
   declarations: [AppComponent, HeroDetailComponent, HeroesComponent,
-    DashboardComponent],
+    DashboardComponent, HeroSearchComponent],
   // For injected services. Can be at any level but in module it is a singleton
   providers: [HeroService],
   bootstrap: [AppComponent]
